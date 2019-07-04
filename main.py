9dc4b11d2fd1d5dec827d0b87fcda76293af9050
Pablo Andrés Díaz Mazzaro <diazmazzaro@gmail.com>
2019-07-04 20:39:51 -0300

    First hw

---
+++ main.py
@@ -0,0 +1,41 @@
+#  __    __  ____    __    ____      __  
+# |  |  |  | \   \  /  \  /   /     /_ | 
+# |  |__|  |  \   \/    \/   / ______| | 
+# |   __   |   \            / |______| | 
+# |  |  |  |    \    /\    /         | | 
+# |__|  |__|     \__/  \__/          |_| 
+# ---------------------------------------------------------
+
+# Global variables.
+# Song's title
+Title         = "All the small things"
+# Song's album
+Album         = "Enema of the State"
+# Song's artist
+Artist        = "Blink-182"
+# Song's record label
+Label         = "MCA Records"
+# Song'genre
+Genre         = "IPunk rock"
+# Song's duration in minutes
+DurationInMin = 2.8
+# Album's yaer
+Year          = 1999
+# Track number in album
+TrackNum      = 8
+
+
+
+# Print al varliables
+print()
+print("--------------------------")
+print("Title: ", Title)
+print("--------------------------")
+print("Album: ", Album)
+print("Artist: ", Artist)
+print("Label: ", Label)
+print("Genre: ", Genre)
+print("DurationInMin: ", DurationInMin)
+print("Year: ", Year)
+print("Track: ", TrackNum)
+print("--------------------------")
